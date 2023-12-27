5685c285ea2aebddea960afb62bd15ca74ee24fd
SvdNeo <sanjay.d@neoito.com>
2023-12-27 18:27:57 +0530

    optional catch binding

--- error_handling.js
+++ error_handling.js
@@ -57,3 +57,18 @@ try {
     // The error is converted to string as "name: message"
     console.log(err); // ReferenceError: lalala is not defined
   }
+
+  //optional catch binding 
+  let json = "{ bad json }";
+
+try {
+
+  let user = JSON.parse(json); // <-- when an error occurs...
+  console.log( user.name ); // doesn't work
+
+} catch (err) {
+  // ...the execution jumps here
+  console.log( "Our apologies, the data has errors, we'll try to request it one more time." );
+  console.log( err.name );
+  console.log( err.message );
+}

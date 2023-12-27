20bf999e41f8d8eba81116cbb5174a497c6c4e38
SvdNeo <sanjay.d@neoito.com>
2023-12-27 17:53:24 +0530

    error handling examples

---
+++ error_handling.js
@@ -0,0 +1,19 @@
+//example1 try without error
+try{
+    console.log("start")
+
+    console.log("end")
+}
+catch{
+    console.log("No error")
+}
+
+// try with error 
+try{
+    console.log('start')
+    console.log(try)
+    console.log('end')
+}
+catch(error){
+    console.log('Error occurred')
+}

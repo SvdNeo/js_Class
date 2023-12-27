409a51f202ce047ccb856c42e12d1836170a9564
SvdNeo <sanjay.d@neoito.com>
2023-12-27 18:15:39 +0530

    error object

--- error_handling.js
+++ error_handling.js
@@ -44,3 +44,16 @@ console.log(`error`)
 finally{
     console.log(`end the loop`)
 }
+
+//error object
+try {
+    lalala; // error, variable is not defined!
+  } catch (err) {
+    console.log(err.name); // ReferenceError
+    console.log(err.message); // lalala is not defined
+    console.log(err.stack); // ReferenceError: lalala is not defined at (...call stack)
+  
+    // Can also show an error as a whole
+    // The error is converted to string as "name: message"
+    console.log(err); // ReferenceError: lalala is not defined
+  }
